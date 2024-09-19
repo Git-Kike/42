96e1a7e889f50227536423232f0f04d8852e6d77
Git-Kike <312196@students.esic.edu>
2024-09-19 12:31:06 +0200

    bzero

---
+++ libft_github/ft_ bzero.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ bzero.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 11:58:40 by marvin            #+#    #+#             */
+/*   Updated: 2024/09/19 11:58:40 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+    unsigned char	*ptr;
+    size_t			i;
+
+    ptr = (unsigned char *)s;
+    i = 0;
+    while (i < n)
+    {
+        ptr[i] = 0;
+        i++;
+    }
+}
+
+int main()
+{
+    char str[] = "Hello World!";
+    ft_bzero(str, 5);
+    printf("%s\n", str);
+    return 0;
+}
