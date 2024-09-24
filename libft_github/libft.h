7c307a2c19bc973b88c16469fcc66ac2d90de4fd
Enrique Oliveros Mendizabal <eolivero@c1r12s3.42madrid.com>
2024-09-24 11:36:44 +0200

    XD

--- libft_github/libft.h
+++ libft_github/libft.h
@@ -10,5 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
 
 int ft_isalpha(int c);
+void	ft_bzero(void *s, size_t n);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isdigit(int c);
+int ft_isprint(int c);
+void *ft_memcpy(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+size_t	ft_strlen(const char *s);
+
+#endif
