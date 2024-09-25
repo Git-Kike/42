922afbac39167cd7e33b8722881586aded74898a
Enrique Oliveros Mendizabal <eolivero@c1r13s4.42madrid.com>
2024-09-25 14:46:29 +0200

    tolower

--- libft_github/ft_tolower.c
+++ libft_github/ft_tolower.c
@@ -6,7 +6,27 @@
 /*   By: eolivero <eolivero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 14:16:00 by eolivero          #+#    #+#             */
-/*   Updated: 2024/09/25 14:16:01 by eolivero         ###   ########.fr       */
+/*   Updated: 2024/09/25 14:27:53 by eolivero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+/* #include <stdio.h>
+ */
+int	ft_tolower(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (c + 32);
+	return (c);
+}
+/* int main() {
+    char test_char1 = 'a';
+    char test_char2 = 'Z';
+    char test_char3 = '1';
+
+    printf("'%c' -> Convertido: '%c'\n", test_char1, ft_tolower(test_char1));
+    printf("'%c' -> Convertido: '%c'\n", test_char2, ft_tolower(test_char2));
+    printf("'%c' -> Convertido: '%c'\n", test_char3, ft_tolower(test_char3));
+
+    return 0;
+} */
--- libft_github/libft.h
+++ libft_github/libft.h
@@ -30,5 +30,6 @@ size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
