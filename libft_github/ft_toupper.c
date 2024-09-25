ce7e2ef12c3a99aa43c80076026c26d914112170
Enrique Oliveros Mendizabal <eolivero@c1r13s4.42madrid.com>
2024-09-25 14:16:53 +0200

    toupper

---
+++ libft_github/ft_toupper.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eolivero <eolivero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 13:30:29 by eolivero          #+#    #+#             */
+/*   Updated: 2024/09/25 14:15:32 by eolivero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+/* #include <stdio.h> */
+
+int	ft_toupper(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (c - 32);
+	return (c);
+}
+/* int main() {
+    char test_char1 = 'a';
+    char test_char2 = 'Z';
+    char test_char3 = '1';
+
+    printf("'%c' -> Convertido: '%c'\n", test_char1, ft_toupper(test_char1));
+    printf("'%c' -> Convertido: '%c'\n", test_char2, ft_toupper(test_char2));
+    printf("'%c' -> Convertido: '%c'\n", test_char3, ft_toupper(test_char3));
+
+    return 0;
+} */
--- libft_github/libft.h
+++ libft_github/libft.h
@@ -29,5 +29,6 @@ void	*ft_memset(void *b, int c, size_t len);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		ft_toupper(int c);
 
 #endif
