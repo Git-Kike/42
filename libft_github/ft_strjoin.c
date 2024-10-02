841fc9ae9db75bb1ae594a95ffe3f253488a5bb3
Git-Kike <enriqueoliverosmendizabal@gmail.com>
2024-10-02 19:42:16 +0200

    strjoin ok

---
+++ libft_github/ft_strjoin.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 19:20:13 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/02 19:20:13 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*str;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	i;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = ft_calloc(s1_len + s2_len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < s1_len)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (i < s1_len + s2_len)
+	{
+		str[i] = s2[i - s1_len];
+		i++;
+	}
+	return (str);
+}
+/* int main()
+{
+	char *s1 = "Hello";
+	char *s2 = "World!";
+	printf("%s\n", ft_strjoin(s1, s2));
+	return 0;
+} */
