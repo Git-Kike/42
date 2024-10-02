8c3369466b269bf568d0edce977869935329d3c1
Enrique Oliveros Mendizabal <eolivero@c3r13s5.42madrid.com>
2024-10-02 13:39:20 +0200

    substr sin main

---
+++ libft_github/ft_substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eolivero <eolivero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 13:18:14 by eolivero          #+#    #+#             */
+/*   Updated: 2024/10/02 13:27:44 by eolivero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	size_t	str_len;
+	char	*substr;
+
+	if (!s)
+		return (NULL);
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+		return (ft_strdup(""));
+	if (len > str_len - start)
+		len = str_len - start;
+	substr = ft_calloc(len + 1, sizeof(char));
+	if (!substr)
+		return (NULL);
+	ft_memcpy(substr, s + start, len);
+	return (substr);
+}
