28e20fc1a2ed714928bd97d994c9618f4663de1f
Enrique Oliveros Mendizabal <eolivero@c1r13s2.42madrid.com>
2024-10-04 15:25:57 +0200

    split

--- libft_github/ft_split.c
+++ libft_github/ft_split.c
@@ -14,59 +14,59 @@
 
 static size_t	ft_count_words(const char *s, char c)
 {
-    size_t	count;
-    size_t	i;
+	size_t	count;
+	size_t	i;
 
-    count = 0;
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] != c)
-        {
-            count++;
-            while (s[i] && s[i] != c)
-                i++;
-        }
-        else
-            i++;
-    }
-    return (count);
+	count = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		else
+			i++;
+	}
+	return (count);
 }
 
 static size_t	ft_word_len(const char *s, char c)
 {
-    size_t	len;
+	size_t	len;
 
-    len = 0;
-    while (s[len] && s[len] != c)
-        len++;
-    return (len);
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	return (len);
 }
 
 char	**ft_split(const char *s, char c)
 {
-    char	**tab;
-    size_t	i;
-    size_t	j;
-    size_t	k;
+	char	**tab;
+	size_t	i;
+	size_t	j;
+	size_t	k;
 
-    if (!s || !(tab = ft_calloc(ft_count_words(s, c) + 1, sizeof(char *))))
-        return (NULL);
-    i = 0;
-    j = 0;
-    while (s[i])
-    {
-        if (s[i] != c)
-        {
-            if (!(tab[j] = ft_calloc(ft_word_len(s + i, c) + 1, sizeof(char))))
-                return (NULL);
-            k = 0;
-            while (s[i] && s[i] != c)
-                tab[j][k++] = s[i++];
-            j++;
-        }
-        else
-            i++;
-    }
-    return (tab);
+	if (!s || !(tab = ft_calloc(ft_count_words(s, c) + 1, sizeof(char *))))
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			if (!(tab[j] = ft_calloc(ft_word_len(s + i, c) + 1, sizeof(char))))
+				return (NULL);
+			k = 0;
+			while (s[i] && s[i] != c)
+				tab[j][k++] = s[i++];
+			j++;
+		}
+		else
+			i++;
+	}
+	return (tab);
 }
